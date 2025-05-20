d9d42fb68a9b569932920ebc384cb389ba38af27
swastik-git-bit <swastik231206@gmail.com>
2025-05-21 02:06:22 +0530

    Create Eaxmple_file.py

---
+++ Q5/Eaxmple_file.py
@@ -0,0 +1,9 @@
+grid1 = ["@..a.",
+         "###.#",
+         "b.A.B"]
+print(min_moves_to_collect_all_artifacts(grid1)) 
+# Output: 8
+
+grid2 = ["@Aa"]
+print(min_moves_to_collect_all_artifacts(grid2))  
+# Output: -1
